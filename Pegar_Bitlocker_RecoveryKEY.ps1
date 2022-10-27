547687cfc85045f4fd03ee34706e960933db96ed
Eliascsilveira <83977207+Eliascsilveira@users.noreply.github.com>
2022-10-27 07:57:37 -0300

    Create Pegar_Bitlocker_RecoveryKEY.ps1

---
+++ Pegar_Bitlocker_RecoveryKEY.ps1
@@ -0,0 +1,14 @@
+$objComputer = Get-ADComputer WLGBROD0096I -Server DRIO01B1.linde.lds.grp
+$Bitlocker_Object = Get-ADObject -Server DRIO01B1.linde.lds.grp -Filter {objectclass -eq 'msFVE-RecoveryInformation'}  -SearchBase $objComputer.DistinguishedName -Properties 'msFVE-RecoveryPassword' 
+$Bitlocker_Object 
+
+$LocalAdmin = $objComputer
+
+$LocalAdmin.AdmPwd = $(Get-ADComputer $objComputer -Properties ms-MCS-AdmPwd | select -ExpandProperty ms-MCS-AdmPwd)
+
+$comp =  Get-ADComputer $objComputer  -Property * | Format-Table ms-Mcs-AdmPwd,OperatingSystem,OperatingSystemVersion,DNSHostName,IPv4Address , primaryTelexNumber,  Description -Wrap –Auto  
+
+$comp | ft -AutoSize
+
+$Bitlocker_Object | Out-File -FilePath \\brariofs01\transfer\elias\BitLock.txt
+$comp | Out-File -FilePath \\brariofs01\transfer\elias\senha.txt
