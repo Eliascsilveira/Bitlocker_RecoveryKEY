eb30f97df9ebe523e7c5b793823be23e099118f1
Eliascsilveira <83977207+Eliascsilveira@users.noreply.github.com>
2022-10-29 11:21:38 -0300

    Update TodososdadosdoUsuario.ps1

--- TodososdadosdoUsuario.ps1
+++ TodososdadosdoUsuario.ps1
@@ -1,4 +1,4 @@
-$user = Get-ADUser h1th25  -Server DRIO01B1.linde.lds.grp -Properties * 
+$user = Get-ADUser e1zq74  -Server DRIO01B1.linde.lds.grp -Properties * 
 
 $user 
 $user | Out-File -FilePath \\brariofs01\transfer\elias\usuario.txt
